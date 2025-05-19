25e21590ba8dbe4b7060766f744e479e7ef3b033
jakucho97 <jkchobot@gmail.com>
2025-05-19 16:55:35 +0200

    [620] [Time Beats: 29.21%] - LeetPush

---
+++ 620-not-boring-movies/Not-Boring-Movies.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+def not_boring_movies(cinema: pd.DataFrame) -> pd.DataFrame:
+    
+    return cinema[(cinema['description']!='boring')&(cinema['id']%2!=0)].sort_values(by=['rating'], ascending=False)
